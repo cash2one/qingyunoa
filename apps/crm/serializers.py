9faa2a92185d6f886a2f3d64d07f1e77a5e6b857
lyhapple <liyinhuiapple@163.com>
2015-12-24 09:41:57 +0800

    ProductSerializer

--- apps/crm/serializers.py
+++ apps/crm/serializers.py
@@ -6,6 +6,14 @@ __author__ = 'lyhapple'
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    ptype = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+
+    def get_ptype(self, obj):
+        return obj.ptype.value
+
+    def get_status(self, obj):
+        return obj.status.value
 
     class Meta:
         model = Product
